855301f3efc264737bffa75b192ff962fee1d7dc
Karchaou Mohammed <mohamedkarchaou4@gmail.com>
2025-05-19 11:52:07 +0100

    add credentials to authService

--- src/services/authService.jsx
+++ src/services/authService.jsx
@@ -1,7 +1,11 @@
-import API from '../services/api';
+import API from "../services/api";
 
 export async function loginUser(email, password) {
-  const response = await API.post('/login', { email, password });
+  const response = await API.post(
+    "/login",
+    { email, password },
+    { withCredentials: true }
+  );
   const { utilisateur, token } = response.data;
   return { user: utilisateur, token };
 }
